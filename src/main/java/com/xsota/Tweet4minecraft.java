f05b5f459069872ec796a1928cf28d0f3ec0a034
sota <git@xsota.com>
2015-12-01 15:04:57 +0900

    プラグインが有効になった際にconfig.ymlを生成するように

--- src/main/java/com/xsota/Tweet4minecraft.java
+++ src/main/java/com/xsota/Tweet4minecraft.java
@@ -1,5 +1,6 @@
 package com.xsota;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -7,7 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Tweet4minecraft extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		getLogger().info("plugin has been enable.");			
+		getLogger().info("plugin has been enable.");
+		this.saveDefaultConfig();
+		
+		FileConfiguration config = this.getConfig();
+		//this.getConfig().set("API.KEY", "SuucnlrR6yAOXz3AQaJG4Q");
+		//this.getConfig().set("API.SECRET", "wCL1YIuGH8fIKvBfXKrBbacGF8mhJHBR3A11MpmyPw");
+		
+		//if(config.get("API.KEY"))
+	    
+		this.saveConfig();
 	}
 	
 	@Override
