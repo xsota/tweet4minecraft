41a68cedfb621b581c5815d60f9d6be7a3f6abb0
sota <git@xsota.com>
2015-12-01 09:13:23 +0900

    tweetコマンドの処理かくとこ

---
+++ src/main/java/commands/Tweet.java
@@ -0,0 +1,12 @@
+package commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Tweet implements CommandExecutor{
+	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] arg3) {
+		
+		return false;
+	}
+}
