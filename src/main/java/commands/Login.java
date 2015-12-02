47ca0c864570e371f2ad33f8b6ebdd3c80854a16
sota <git@xsota.com>
2015-12-02 11:20:59 +0900

    loginコマンドでリクエストurlが表示されるまで実装

--- src/main/java/commands/Login.java
+++ src/main/java/commands/Login.java
@@ -1,18 +1,32 @@
 package commands;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 
-public class Login implements CommandExecutor{
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+public class Login implements CommandExecutor {
 	FileConfiguration config;
 	String apiKey;
 	String apiSecret;
-	String oauthToken ;
+	String oauthToken;
 	String oauthTokenSecret;
-	
+
 	public Login(FileConfiguration config) {
 		this.config = config;
 		apiKey = config.getString("API.KEY");
@@ -20,7 +34,21 @@ public class Login implements CommandExecutor{
 	}
 
 	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] arg3) {
-		Bukkit.broadcastMessage("apiKey:"+apiKey);
-		return false;
+		AccessToken accessToken = null;
+		RequestToken requestToken = null;
+
+		Twitter twitter = TwitterFactory.getSingleton();
+
+		twitter.setOAuthConsumer(apiKey, apiSecret);
+
+		try {
+			requestToken = twitter.getOAuthRequestToken();
+		} catch (TwitterException e) {
+			// そのうちエラー処理書くよ
+		}
+		Bukkit.broadcastMessage("ログインコマンド");
+		Bukkit.broadcastMessage("ログインしてね" + requestToken.getAuthorizationURL());
+
+		return true;
 	}
 }
--- src/main/java/commands/Tweet.java
+++ src/main/java/commands/Tweet.java
@@ -1,12 +1,26 @@
 package commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class Tweet implements CommandExecutor{
+	FileConfiguration config;
+	String apiKey;
+	String apiSecret;
+	String oauthToken ;
+	String oauthTokenSecret;
+	
+	public Tweet(FileConfiguration config) {
+		this.config = config;
+		apiKey = config.getString("API.KEY");
+		apiSecret = config.getString("API.SECRET");
+	}
+
 	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] arg3) {
-		
+		Bukkit.broadcastMessage("apiKey:" + apiKey);
 		return false;
 	}
 }
